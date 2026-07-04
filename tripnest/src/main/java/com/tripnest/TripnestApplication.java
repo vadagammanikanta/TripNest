de9581d66b9816a0cab51f0d59bf6e0abd88048f
vadagammanikanta <vadagammanikanta@github.com>
2026-07-04 21:22:48 +0530

    feat: auto-seed roles table with ROLE_TRAVELER, ROLE_AGENT, ROLE_ADMIN on startup

--- tripnest/src/main/java/com/tripnest/TripnestApplication.java
+++ tripnest/src/main/java/com/tripnest/TripnestApplication.java
@@ -3,10 +3,29 @@ package com.tripnest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import com.tripnest.entity.ERole;
+import com.tripnest.entity.Role;
+import com.tripnest.repository.RoleRepository;
+
 @SpringBootApplication
 public class TripnestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TripnestApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(RoleRepository roleRepository) {
+        return args -> {
+            for (ERole roleName : ERole.values()) {
+                if (roleRepository.findByName(roleName).isEmpty()) {
+                    Role role = new Role();
+                    role.setName(roleName);
+                    roleRepository.save(role);
+                }
+            }
+        };
     }
 }
